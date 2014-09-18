c2366d785300430583bbef402659f4906ff82ffc
Matthew Kevins <mmkevins@yahoo.com>
2014-09-18 12:00:39 -0400

    implement king, opponent, move_diffs methods on Piece

--- piece.rb
+++ piece.rb
@@ -4,10 +4,15 @@ class Piece
 
   attr_reader :color
 
+  def inspect
+    "#{self.color} piece"
+  end
+
   def initialize(board, position, color)
     @board = board
     @color = color
     board[position] = self
+    @is_king = false
   end
 
   def perform_slide
@@ -16,6 +21,14 @@ class Piece
 
   def perform_jump
 
+  end
+
+  def king_me
+    @is_king = true
+  end
+
+  def king?
+    @is_king
   end
 
   def display
@@ -27,4 +40,22 @@ class Piece
     end
   end
 
+  private
+
+  def move_diffs
+    direction = {red: 1,   black: -1}[self.color]
+    diffs = [[-1, direction], [1, direction]]
+    if king?
+      diffs += [[-1, -direction], [1, -direction]]
+    end
+
+    diffs
+  end
+
+  def opponent
+    self.color == :red ? :black : :red
+  end
+
+
 end
+
