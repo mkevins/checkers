efbc17748421114bd975c0386dabd12ebcd5dd63
Matthew Kevins <mmkevins@yahoo.com>
2014-09-18 11:37:14 -0400

    add Board class; implement simple display methods

---
+++ board.rb
@@ -0,0 +1,55 @@
+require 'colorize'
+require_relative 'piece'
+
+class Board
+
+  def initialize
+    @grid = Array.new(8) { Array.new(8) }
+    place_pieces
+  end
+
+  def [](position)
+    x, y = position
+    @grid[y][x]
+  end
+
+  def []=(position, piece)
+    x, y = position
+    @grid[y][x] = piece
+  end
+
+  def display
+    @grid.each do |row|
+      row.map do |piece|
+        if piece.nil?
+          print " ".white.on_light_black
+        else
+          piece.display
+        end
+      end
+      print "\n"
+    end
+
+    nil
+  end
+
+  private
+
+  def place_pieces
+    # red pieces
+    (0..3).each do |i|
+      Piece.new(self, [i * 2, 0], :red)
+      Piece.new(self, [i * 2 + 1, 1], :red)
+      Piece.new(self, [i * 2, 2], :red)
+    end
+
+    # white pieces
+    (0..3).each do |i|
+      Piece.new(self, [i * 2 + 1, 5], :white)
+      Piece.new(self, [i * 2, 6], :white)
+      Piece.new(self, [i * 2 + 1, 7], :white)
+    end
+
+  end
+
+end
--- piece.rb
+++ piece.rb
@@ -1,4 +1,14 @@
-class piece
+require 'colorize'
+
+class Piece
+
+  attr_reader :color
+
+  def initialize(board, position, color)
+    @board = board
+    @color = color
+    board[position] = self
+  end
 
   def perform_slide
 
@@ -6,6 +16,15 @@ class piece
 
   def perform_jump
 
+  end
+
+  def display
+    case self.color
+    when :red
+      print "*".red.on_white
+    when :white
+      print "*".black.on_white
+    end
   end
 
 end
